3e14108b7af8bea55d6c613888ed950e12574cb9
Alexandre Herman <athermandev@gmail.com>
2024-07-09 14:37:59 -0300

    created is_there_record(file)

--- third_semester/data_recovery/file_index/create_index_file.py
+++ third_semester/data_recovery/file_index/create_index_file.py
@@ -1,5 +1,12 @@
+import os
+
 def main():
     file = open('trabalhos.dat','rb')
     index_file = open('index_file.dat','xb')
     while is_there_record(file):
         write_record(index_file,read_record(file))
+
+def is_there_record(file) -> str:
+    next_byte_decoded = file.read(1).decode()
+    file.seek(-1,os.SEEK_CUR)
+    return next_byte_decoded
