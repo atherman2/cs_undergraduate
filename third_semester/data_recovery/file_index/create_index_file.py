1bdcc943daf524659550e6d2b5fa181f60940353
Alexandre Herman <athermandev@gmail.com>
2024-07-09 15:09:58 -0300

    Update create_index_file.py
    few changes and added read_record(file, current_byte_offset)

--- third_semester/data_recovery/file_index/create_index_file.py
+++ third_semester/data_recovery/file_index/create_index_file.py
@@ -3,9 +3,11 @@ import os
 def main() -> None:
     file = open('trabalhos.dat','rb')
     index_file = open('index_file.dat','xb')
-    record_header = 4
-    file.seek(record_header,os.SEEK_SET)
-    next_byte_offset = record_header
+    current_byte_offset = 0
+    record_header_size = 4
+    file.seek(record_header_size,os.SEEK_SET)
+    current_byte_offset += record_header_size
+    next_byte_offset = current_byte_offset
     while is_there_record(file):
         current_byte_offset = next_byte_offset
         record, next_byte_offset = read_record(file, current_byte_offset)
@@ -18,3 +20,14 @@ def is_there_record(file) -> str:
     next_byte_decoded = file.read(1).decode()
     file.seek(-1,os.SEEK_CUR)
     return next_byte_decoded
+
+def read_record(file, current_byte_offset):
+    # receives the current_byte_offset and reads a record
+    # returns the record read and the next_byte_offset
+    next_byte_offset = current_byte_offset
+    bin_record_size = file.read(2)
+    next_byte_offset += 2
+    record_size = int.from_bytes(bin_record_size)
+    record = file.read(record_size)
+    next_byte_offset += record_size
+    return record, next_byte_offset
